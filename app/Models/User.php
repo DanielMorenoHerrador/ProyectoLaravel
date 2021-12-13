5c1e47f3a5f6d090a2b7afb3cafc165a330110c1
DanielMorenoHerrador <90707206+DanielMorenoHerrador@users.noreply.github.com>
2021-12-13 03:37:56 +0100

    Actualizacion en el model de user

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 
 class User extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable, CrudTrait;
 
     //
     protected $fillable = [
