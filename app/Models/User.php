fdb9fa0d7cf5ca9d56d67c3069fd771fe6222a55
DanielMorenoHerrador <90707206+DanielMorenoHerrador@users.noreply.github.com>
2021-12-13 03:41:46 +0100

    Actualizacion en el model de user

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
 
 class User extends Model
